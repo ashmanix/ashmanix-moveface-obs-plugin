f535f6352890276edcbdb8cc388eb3d9287189a8
ashmanix <theashmanix@gmail.com>
2025-01-04 01:50:22 +0000

    clang changes

--- build-aux/.run-format.zsh
+++ build-aux/.run-format.zsh
@@ -49,10 +49,10 @@ invoke_formatter() {
         exit 2
       fi
 
-      if ! is-at-least ${formatter_version[-1]} 17.0.3; then
-        log_error "clang-format is more recent than version 17.0.3 (found ${formatter_version[-1]})."
+      # if ! is-at-least ${formatter_version[-1]} 17.0.3; then
+        # log_error "clang-format is more recent than version 17.0.3 (found ${formatter_version[-1]})."
         # exit 2
-      fi
+      # fi
 
       if (( ! #source_files )) source_files=(src/**/*.(c|cpp|h|hpp|m|mm)(.N))
 
