21dc495f08d5b0d0fa4e419cabdf8567d05c84e4
ashmanix <theashmanix@gmail.com>
2025-02-03 22:57:43 +0000

    Refactored code

--- src/obs/my-gs-texture-wrapper.h
+++ src/obs/my-gs-texture-wrapper.h
@@ -1,16 +1,15 @@
 #pragma once
 
+#include <graphics/graphics.h>
+#include <QMetaType>
+
 // This wrapper only exists to get round the issue with Qt not being
 // able to get the size of a gs_texture_t because it's full details
-// are not exposed by OBS
-#include <graphics/graphics.h>
-
-// Our wrapper hides the opaque gs_texture pointer.
+// are not exposed by OBS. It hides the opaque gs_texture pointer.
 class MyGSTextureWrapper {
 public:
-	MyGSTextureWrapper(gs_texture_t *tex = nullptr) : texture(tex) {}
+	explicit MyGSTextureWrapper(gs_texture_t *tex = nullptr) : texture(tex) {}
 
-	// You can add methods to update or retrieve the texture as needed.
 	gs_texture_t *get() const { return texture; }
 	void set(gs_texture_t *tex) { texture = tex; }
 
@@ -19,5 +18,4 @@ private:
 };
 
 // Register our type with Qt.
-#include <QMetaType>
 Q_DECLARE_METATYPE(MyGSTextureWrapper)
